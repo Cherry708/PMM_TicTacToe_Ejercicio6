8222801eec9ba6dcbfa78ba239c9f11fa937cb7b
Cherry708 <aluser8241@ieselcaminas.org>
2021-10-28 20:39:49 +0200

    Finalizado

--- app/src/main/java/com/example/pmm_tictactoe_ejercicio6/MainActivity.kt
+++ app/src/main/java/com/example/pmm_tictactoe_ejercicio6/MainActivity.kt
@@ -19,6 +19,18 @@ class MainActivity : AppCompatActivity() {
             binding.tvTurno.text = getString(R.string.turno_x)
         } else binding.tvTurno.text = getString(R.string.turno_o)
 
+        val listaBotones = ArrayList<Button>()
+
+        listaBotones.add(binding.btn1)
+        listaBotones.add(binding.btn2)
+        listaBotones.add(binding.btn3)
+        listaBotones.add(binding.btn4)
+        listaBotones.add(binding.btn5)
+        listaBotones.add(binding.btn6)
+        listaBotones.add(binding.btn7)
+        listaBotones.add(binding.btn8)
+        listaBotones.add(binding.btn9)
+
         binding.btn1.setOnClickListener {
             if (turno) {
                 binding.btn1.text = "X"
@@ -32,7 +44,7 @@ class MainActivity : AppCompatActivity() {
             } else binding.tvTurno.text = getString(R.string.turno_o)
 
             binding.btn1.isEnabled = false
-            condiciones()
+            condiciones(listaBotones)
         }
 
         binding.btn2.setOnClickListener {
@@ -47,7 +59,7 @@ class MainActivity : AppCompatActivity() {
                 binding.tvTurno.text = getString(R.string.turno_x)
             } else binding.tvTurno.text = getString(R.string.turno_o)
             binding.btn2.isEnabled = false
-            condiciones()
+            condiciones(listaBotones)
         }
 
         binding.btn3.setOnClickListener {
@@ -62,7 +74,7 @@ class MainActivity : AppCompatActivity() {
                 binding.tvTurno.text = getString(R.string.turno_x)
             } else binding.tvTurno.text = getString(R.string.turno_o)
             binding.btn3.isEnabled = false
-            condiciones()
+            condiciones(listaBotones)
         }
 
         binding.btn4.setOnClickListener {
@@ -77,6 +89,7 @@ class MainActivity : AppCompatActivity() {
                 binding.tvTurno.text = getString(R.string.turno_x)
             } else binding.tvTurno.text = getString(R.string.turno_o)
             binding.btn4.isEnabled = false
+            condiciones(listaBotones)
         }
 
         binding.btn5.setOnClickListener {
@@ -91,6 +104,7 @@ class MainActivity : AppCompatActivity() {
                 binding.tvTurno.text = getString(R.string.turno_x)
             } else binding.tvTurno.text = getString(R.string.turno_o)
             binding.btn5.isEnabled = false
+            condiciones(listaBotones)
         }
 
         binding.btn6.setOnClickListener {
@@ -105,6 +119,7 @@ class MainActivity : AppCompatActivity() {
                 binding.tvTurno.text = getString(R.string.turno_x)
             } else binding.tvTurno.text = getString(R.string.turno_o)
             binding.btn6.isEnabled = false
+            condiciones(listaBotones)
         }
 
         binding.btn7.setOnClickListener {
@@ -119,6 +134,7 @@ class MainActivity : AppCompatActivity() {
                 binding.tvTurno.text = getString(R.string.turno_x)
             } else binding.tvTurno.text = getString(R.string.turno_o)
             binding.btn7.isEnabled = false
+            condiciones(listaBotones)
         }
 
         binding.btn8.setOnClickListener {
@@ -133,6 +149,7 @@ class MainActivity : AppCompatActivity() {
                 binding.tvTurno.text = getString(R.string.turno_x)
             } else binding.tvTurno.text = getString(R.string.turno_o)
             binding.btn8.isEnabled = false
+            condiciones(listaBotones)
         }
 
         binding.btn9.setOnClickListener {
@@ -147,80 +164,116 @@ class MainActivity : AppCompatActivity() {
                 binding.tvTurno.text = getString(R.string.turno_x)
             } else binding.tvTurno.text = getString(R.string.turno_o)
             binding.btn9.isEnabled = false
+            condiciones(listaBotones)
         }
     }
 
-    fun condiciones() {
-
-        println("Mostrar")
+    fun condiciones(listaBotones : ArrayList<Button>) {
 
         val tvResultado = findViewById<TextView>(R.id.tvResultado)
 
-        val btn1 = findViewById<Button>(R.id.btn1)
-        val btn2 = findViewById<Button>(R.id.btn2)
-        val btn3 = findViewById<Button>(R.id.btn3)
-        val btn4 = findViewById<Button>(R.id.btn4)
-        val btn5 = findViewById<Button>(R.id.btn5)
-        val btn6 = findViewById<Button>(R.id.btn6)
-        val btn7 = findViewById<Button>(R.id.btn7)
-        val btn8 = findViewById<Button>(R.id.btn8)
-        val btn9 = findViewById<Button>(R.id.btn9)
-
-        val text1 = btn1.text.toString()
-        val text2 = btn2.text.toString()
-        val text3 = btn3.text.toString()
-        val text4 = btn4.text.toString()
-        val text5 = btn5.text.toString()
-        val text6 = btn6.text.toString()
-        val text7 = btn7.text.toString()
-        val text8 = btn8.text.toString()
-        val text9 = btn9.text.toString()
-
+        val text1 = listaBotones.get(0).text.toString()
+        val text2 = listaBotones.get(1).text.toString()
+        val text3 = listaBotones.get(2).text.toString()
+        val text4 = listaBotones.get(3).text.toString()
+        val text5 = listaBotones.get(4).text.toString()
+        val text6 = listaBotones.get(5).text.toString()
+        val text7 = listaBotones.get(6).text.toString()
+        val text8 = listaBotones.get(7).text.toString()
+        val text9 = listaBotones.get(8).text.toString()
 
         when {
             //Filas
-            text1 == "X" && text2 == "X" && text3 == "X" ->
+            text1 == "X" && text2 == "X" && text3 == "X" -> {
                 tvResultado.text = resources.getString(R.string.victoria_x)
-            text4 == "X" && text5 == "X" && text6 == "X" ->
+                for(boton in listaBotones)
+                    boton.isEnabled = false
+            }
+            text4 == "X" && text5 == "X" && text6 == "X" -> {
                 tvResultado.text = resources.getString(R.string.victoria_x)
-            text7 == "X" && text8 == "X" && text9 == "X" ->
+                for(boton in listaBotones)
+                    boton.isEnabled = false
+            }
+            text7 == "X" && text8 == "X" && text9 == "X" -> {
                 tvResultado.text = resources.getString(R.string.victoria_x)
+                for(boton in listaBotones)
+                    boton.isEnabled = false
+            }
 
             //Columnas
-            text1 == "X" && text4 == "X" && text7 == "X" ->
+            text1 == "X" && text4 == "X" && text7 == "X" -> {
                 tvResultado.text = resources.getString(R.string.victoria_x)
-            text2 == "X" && text5 == "X" && text8 == "X" ->
+                for(boton in listaBotones)
+                    boton.isEnabled = false
+            }
+            text2 == "X" && text5 == "X" && text8 == "X" -> {
                 tvResultado.text = resources.getString(R.string.victoria_x)
-            text3 == "X" && text6 == "X" && text9 == "X" ->
+                for(boton in listaBotones)
+                    boton.isEnabled = false
+            }
+            text3 == "X" && text6 == "X" && text9 == "X" -> {
                 tvResultado.text = resources.getString(R.string.victoria_x)
+                for(boton in listaBotones)
+                    boton.isEnabled = false
+            }
 
             //Diagonales
-            text1 == "X" && text5 == "X" && text9 == "X" ->
+            text1 == "X" && text5 == "X" && text9 == "X" -> {
                 tvResultado.text = resources.getString(R.string.victoria_x)
-            text3 == "X" && text5 == "X" && text7 == "X" ->
+                for(boton in listaBotones)
+                    boton.isEnabled = false
+            }
+            text3 == "X" && text5 == "X" && text7 == "X" -> {
                 tvResultado.text = resources.getString(R.string.victoria_x)
+                for(boton in listaBotones)
+                    boton.isEnabled = false
+            }
 
             //Filas
-            text1 == "O" && text2 == "O" && text3 == "O" ->
+            text1 == "O" && text2 == "O" && text3 == "O" -> {
                 tvResultado.text = resources.getString(R.string.victoria_o)
-            text4 == "O" && text5 == "O" && text6 == "O" ->
+                for(boton in listaBotones)
+                    boton.isEnabled = false
+            }
+            text4 == "O" && text5 == "O" && text6 == "O" -> {
                 tvResultado.text = resources.getString(R.string.victoria_o)
-            text7 == "O" && text8 == "O" && text9 == "O" ->
+                for(boton in listaBotones)
+                    boton.isEnabled = false
+            }
+            text7 == "O" && text8 == "O" && text9 == "O" -> {
                 tvResultado.text = resources.getString(R.string.victoria_o)
+                for(boton in listaBotones)
+                    boton.isEnabled = false
+            }
 
             //Columnas
-            text1 == "O" && text4 == "O" && text7 == "O" ->
+            text1 == "O" && text4 == "O" && text7 == "O" -> {
                 tvResultado.text = resources.getString(R.string.victoria_o)
-            text2 == "O" && text5 == "O" && text8 == "O" ->
+                for(boton in listaBotones)
+                    boton.isEnabled = false
+            }
+            text2 == "O" && text5 == "O" && text8 == "O" -> {
                 tvResultado.text = resources.getString(R.string.victoria_o)
-            text3 == "O" && text6 == "O" && text9 == "O" ->
+                for(boton in listaBotones)
+                    boton.isEnabled = false
+            }
+            text3 == "O" && text6 == "O" && text9 == "O" -> {
                 tvResultado.text = resources.getString(R.string.victoria_o)
+                for(boton in listaBotones)
+                    boton.isEnabled = false
+            }
 
             //Diagonales
-            text1 == "O" && text5 == "O" && text9 == "O" ->
+            text1 == "O" && text5 == "O" && text9 == "O" -> {
                 tvResultado.text = resources.getString(R.string.victoria_o)
-            text3 == "O" && text5 == "O" && text7 == "O" ->
+                for(boton in listaBotones)
+                    boton.isEnabled = false
+            }
+            text3 == "O" && text5 == "O" && text7 == "O" -> {
                 tvResultado.text = resources.getString(R.string.victoria_o)
+                for(boton in listaBotones)
+                    boton.isEnabled = false
+            }
         }
     }
 }
